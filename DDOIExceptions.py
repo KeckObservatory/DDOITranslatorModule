231f8a58bc9e565f8105f505af74b621f0d60901
Max Brodheim <max.brodheim@gmail.com>
2022-05-17 13:41:42 -1000

    Stand in class to represent DDOI Exceptions (not implemented yet)

---
+++ DDOIExceptions.py
@@ -0,0 +1,24 @@
+class DDOIArgumentsChangedException(Exception):
+
+    def __init__(self):
+        super(self)
+
+class DDOIKTLTimeoutException(Exception):
+
+    def __init__(self):
+        super(self)
+
+class DDOIMissingArgumentException(Exception):
+
+    def __init__(self):
+        super(self)
+
+class DDOISubsystemInactiveExcpetion(Exception):
+
+    def __init__(self):
+        super(self)
+
+class DDOISubsystemDisabledException(Exception):
+
+    def __init__(self):
+        super(self)

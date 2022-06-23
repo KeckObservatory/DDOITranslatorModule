1079b70184e2a9d41683910399b21b409bb6eb31
= <=>
2022-06-22 14:27:21 -1000

    additional exceptions added for telescope translator

--- ddoitranslatormodule/ddoiexceptions/DDOIExceptions.py
+++ ddoitranslatormodule/ddoiexceptions/DDOIExceptions.py
@@ -1,14 +1,49 @@
 class DDOIArgumentsChangedException(Exception):
     pass
 
+
 class DDOIKTLTimeoutException(Exception):
     pass
+
 
 class DDOIMissingArgumentException(Exception):
     pass
 
+
 class DDOISubsystemInactiveExcpetion(Exception):
     pass
 
+
 class DDOISubsystemDisabledException(Exception):
     pass
+
+
+class DDOIPreConditionNotRun(Exception):
+    def __init__(self, class_name):
+        self.message = f"Precondition was not for {class_name}"
+        super().__init__(self.message)
+
+    def __str__(self):
+        return f'{self.message}'
+
+
+class DDOIInvalidArguments(Exception):
+    pass
+
+
+class DDOIKTLTimeOut(Exception):
+    pass
+
+
+class DDOINoInstDefined(Exception):
+    pass
+
+
+class DDOINotSelectedInstrument(Exception):
+    def __init__(self, current_inst, inst):
+        self.message = f"The selected instrument: {current_inst} is not {inst}"
+        super().__init__(self.message)
+
+    def __str__(self):
+        return f'{self.message}'
+

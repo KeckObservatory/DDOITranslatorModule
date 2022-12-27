4f26141e0b31869338846aac63b6cfa742454158
Max Brodheim <max.brodheim@gmail.com>
2022-12-27 13:56:34 -1000

    add generic script interface

---
+++ ddoitranslatormodule/inst_script.py
@@ -0,0 +1,15 @@
+import sys
+
+server_location = '/net/vm-ddoiserverbuild'
+translator_module_location = "/ddoi/KPFTranslator/default/KPFTranslator"
+linking_table_location = f"{server_location}/{translator_module_location}/kpf/linking_table.yml"
+
+
+# Add the cli script to impport path
+sys.path.add(0, f"{server_location}/ddoi/DDOITranslatorModule/default/DDOITranslatorModule")
+import ddoitranslatormodule.cli_interface as cli
+
+# Add the translator module to the python path
+sys.path.add(0, f"{server_location}/{translator_module_location}")
+
+cli.main(linking_table_location, sys.argv[1:])

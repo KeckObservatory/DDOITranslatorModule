58e4f8f17268c895c076e87500f2c95a75070695
Josh Walawender <joshwalawender@me.com>
2023-04-22 15:42:41 -1000

    Use logdir for KPF

--- ddoitranslatormodule/cli_interface.py
+++ ddoitranslatormodule/cli_interface.py
@@ -191,7 +191,7 @@ def create_logger():
     utnow = datetime.utcnow()
     date = utnow-timedelta(days=1)
     date_str = date.strftime('%Y%b%d').lower()
-    logdir = Path(f"/home/dsibld/logs/{date_str}/cli_logs")
+    logdir = Path(f"/s/sdata1701/KPFTranslator_logs/{date_str}/cli_logs")
     if logdir.exists() is False:
         logdir.mkdir(parents=True)
     LogFileName = logdir / 'cli_interface.log'
